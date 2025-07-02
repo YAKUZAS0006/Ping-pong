91b9aab3c22d36f52a2a343a5c7333fbb0fc5c6b
YAKUZAS0006 <ikorobov33@gmail.com>
2025-07-02 16:45:58 +0300

    v1
    Класс GameSprite
    Фоновая картинка

---
+++ shooter_game.py
@@ -0,0 +1,68 @@
+from pygame import *
+from random import *
+from time import time as tm
+
+class GameSprite(sprite.Sprite):
+    def __init__(self,player_image,player_x,player_y,player_speed,w,h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(w,h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        key_pressed = key.get_pressed()
+        if key_pressed[K_d] and self.rect.x < 610:
+            self.rect.x += self.speed
+        if key_pressed[K_a] and self.rect.x > 0:
+            self.rect.x -= self.speed
+    def fire(self):
+        bullet = Bullet('pea.png',self.rect.centerx,self.rect.top,-15,15,20)
+        bullets.add(bullet)
+
+
+
+
+
+
+
+
+
+
+window = display.set_mode((700,500))
+display.set_caption('Ping pong')
+#задай фон сцены
+background = transform.scale(image.load('galaxy.jpg'),(700,500))
+window.blit(background,(0,0))
+#создай 2 спрайта и размести их на сцене
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+# peashooter = Player('peashooter.png',100,400,10,50,50)
+font.init()
+font1 = font.SysFont('Arial',70)
+win = font1.render('YOU WIN',True,(255,215,0))
+lose = font1.render('YOU LOSE',True,(255,10,10))
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+                    
+
+            
+                
+    if finish != True:
+        window.blit(background,(0,0))
+        
+
+        
+        
+
+    display.update()
+    clock.tick(FPS)
